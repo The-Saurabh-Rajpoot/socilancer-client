0a70b3a42655cfacc6846de9272d735c813c1573
Saurabh rajpoot <90026142+The-Saurabh-Rajpoot@users.noreply.github.com>
2023-07-28 19:13:45 +0530

    Update uploadPicToCloudinary.js

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,8 +4,8 @@ const uploadPic = async media => {
   try {
     const form = new FormData();
     form.append("file", media);
-    form.append("upload_preset", "social_media_app");
-    form.append("cloud_name", "indersingh");
+    form.append("upload_preset", "social_media_Bhusan");
+    form.append("cloud_name", "dyaznet0l");
 
     const res = await axios.post(process.env.CLOUDINARY_URL, form);
     return res.data.secure_url;
